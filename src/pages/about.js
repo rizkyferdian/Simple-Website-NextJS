e41fa7f718acf43cc598fbcd048aee0c4e93e116
Rizky Ferdian <rizkyferdian002@gmail.com>
2023-02-03 14:30:38 +0700

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <h1>About</h1>
+
+            <Link href="/about">
+                Home
+            </Link>
+
+        </>
+    )
+}
+
+export default About
