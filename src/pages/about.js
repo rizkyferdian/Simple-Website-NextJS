6674eb05755ac21914b05a92a324cdca1ea1b592
Rizky Ferdian <rizkyferdian002@gmail.com>
2023-02-04 14:54:03 +0700

    Styling

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 function About() {
     return (
         <>
             <h1>About</h1>
 
-            <Link href="/about">
-                Home
+            <Link legacyBehavior href="/">
+                <a>Home</a>
             </Link>
 
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,40 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <nav className="navbar">
+        <div className="container">
+          <Link legacyBehavior href="/">
+            <a className="logo">Shift.</a>
+          </Link>
 
-      <Link href="/about">
-        About
-      </Link>
+          <ul className="nav-links">
+            <li className="nav-item"><Link href="/">Home</Link></li>
+            <li className="nav-item"><Link href="/about">About</Link></li>
+            <li className="nav-item"><Link href="/portofolio">Portofolio</Link></li>
+            <li className="nav-item"><Link href="/contact">Contact</Link></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* hero */}
+      <section className="hero">
+        <div className="container">
+          <div className="text-wrapper">
+            <h1 className="title">Halo, Saya Rizky Ferdian</h1>
+            <p className="description">Saya seorang front end developer yang menguasai bahasa Html, CSS , Javascript, ReactJS, Next JS dan seorang desain antarmuka pengguna.</p>
+
+            <Link legacyBehavior href="/contact">
+              <a className="cta">Kontak Saya</a>
+            </Link>
+
+
+          </div>
+
+          <div className="image-wrapper">
+            <img src="/Person.png" width="426" height="543" />
+          </div>
+        </div>
+      </section>
 
     </>
   )
